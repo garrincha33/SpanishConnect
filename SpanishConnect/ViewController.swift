ff39c2272104f410c3e79687d79b9b48c2d3c226
Richard Price <mrspondooli@gmail.com>
2018-07-06 10:05:47 +0100

    Initial Commit

---
+++ SpanishConnect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpanishConnect
+//
+//  Created by Richard Price on 06/07/2018.
+//  Copyright © 2018 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
